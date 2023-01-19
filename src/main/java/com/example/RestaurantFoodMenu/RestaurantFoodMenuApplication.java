af627b6f00ef0b2803fe65ef2f0434f45d2ea6b9
puzaan <hakku.stha22@gmail.com>
2023-01-19 20:34:54 +0545

    first comment

---
+++ src/main/java/com/example/RestaurantFoodMenu/RestaurantFoodMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantFoodMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantFoodMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantFoodMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestaurantFoodMenu/RestaurantFoodMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantFoodMenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantFoodMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
