876cdd128216f7f9abd13523a4b945ffee016380
puzaan <hakku.stha22@gmail.com>
2023-01-19 21:49:15 +0545

    basic things

--- src/main/java/com/example/RestaurantFoodMenu/RestaurantFoodMenuApplication.java
+++ src/main/java/com/example/RestaurantFoodMenu/RestaurantFoodMenuApplication.java
@@ -1,7 +1,9 @@
 package com.example.RestaurantFoodMenu;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestaurantFoodMenuApplication {
@@ -10,4 +12,8 @@ public class RestaurantFoodMenuApplication {
 		SpringApplication.run(RestaurantFoodMenuApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
